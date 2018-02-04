3485117fadc7acf72b3e62715517b42d7d40c0dc
Vishal <vishalcseng@gmail.com>
2018-02-05 01:24:39 +0530

    First testing sucessful with "Toast".

---
+++ app/src/main/java/com/example/hp/quizapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.hp.quizapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends Activity {
+
+    // TODO: Declare constants here
+
+
+    // TODO: Declare member variables here:
+    Button mTrueButton;
+    Button mFalsebutton;
+
+
+    // TODO: Uncomment to create question bank
+//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+//            new TrueFalse(R.string.question_1, true),
+//            new TrueFalse(R.string.question_2, true),
+//            new TrueFalse(R.string.question_3, true),
+//            new TrueFalse(R.string.question_4, true),
+//            new TrueFalse(R.string.question_5, true),
+//            new TrueFalse(R.string.question_6, false),
+//            new TrueFalse(R.string.question_7, true),
+//            new TrueFalse(R.string.question_8, false),
+//            new TrueFalse(R.string.question_9, true),
+//            new TrueFalse(R.string.question_10, true),
+//            new TrueFalse(R.string.question_11, false),
+//            new TrueFalse(R.string.question_12, false),
+//            new TrueFalse(R.string.question_13,true)
+//    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalsebutton = (Button) findViewById(R.id.false_button);
+
+        View.OnClickListener myListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"True Pressed",Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        mTrueButton.setOnClickListener(myListener);
+
+        mFalsebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast myToast = Toast.makeText(getApplicationContext(),"False Pressed",Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+    }
+}
