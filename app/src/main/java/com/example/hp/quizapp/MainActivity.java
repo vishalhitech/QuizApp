9b37720414314cc0629b7a3e663a76e837256ee6
Vishal <vishalcseng@gmail.com>
2018-02-05 04:25:22 +0530

    Proper working with Screen Orientation.

--- app/src/main/java/com/example/hp/quizapp/MainActivity.java
+++ app/src/main/java/com/example/hp/quizapp/MainActivity.java
@@ -47,6 +47,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(savedInstanceState != null){
+            mScore=savedInstanceState.getInt("ScoreKey");
+            mIndex=savedInstanceState.getInt("IndexKey");
+        }else{
+            mScore=0;
+            mIndex=0;
+        }
+
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalsebutton = (Button) findViewById(R.id.false_button);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -109,6 +117,13 @@ public class MainActivity extends Activity {
         else {
             Toast.makeText(getApplicationContext(),R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt("ScoreKey",mScore);
+        outState.putInt("IndexKey",mIndex);
     }
 
 }
